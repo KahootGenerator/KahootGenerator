39a72dd732718a6ebf881aa08b3ba857bdeb9457
corbiobio <edarnand@edenschool.fr>
2024-06-05 14:28:45 +0200

    fix title and remove default file in layout

--- src/Views/layout/layout.php
+++ src/Views/layout/layout.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style/main.css">
-    <title><?= "- ".$data['pageTitle'] ?? "MVC Project" ?></title>
+    <title><?= isset($data['title']) ? "Kahoot Generator - " . $data['title'] : "Kahoot Generator" ?></title>
 </head>
+
 <body>
     <header>
         <img class="logo" src="/img/logo.png" alt="">
@@ -21,4 +23,5 @@
         <?= $content ?>
     </main>
 </body>
+
 </html>
