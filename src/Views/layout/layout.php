241da39b100aad5f37e04dbde3484127455b3a12
BluffDragon8211 <lortega@edenschool.fr>
2024-06-05 15:32:55 +0200

    Changing favicon path

--- src/Views/layout/layout.php
+++ src/Views/layout/layout.php
@@ -7,7 +7,7 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style/main.css">
-    <link rel="shortcut icon" href="/public/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <title><?= isset($data['title']) ? "Kahoot Generator - " . $data['title'] : "Kahoot Generator" ?></title>
 </head>
 
