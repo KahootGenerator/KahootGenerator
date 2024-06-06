0434b4ec3e47bcddc14b9d8c98af8cee5d809abd
IMTR0J4N <jjorgebeau@edenschool.fr>
2024-06-06 14:34:21 +0200

    created User Manager

---
+++ src/Database/Managers/UserManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Managers;
+
+use App\Core\Manager;
+use App\Database\Models\User;
+
+class UserManager extends Manager
+{
+    public function __construct() {
+
+    }
+}
