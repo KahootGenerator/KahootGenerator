b05f06408561d641ff610105d7779e91ea51a037
IMTR0J4N <jjorgebeau@edenschool.fr>
2024-06-06 16:09:05 +0200

    Implement User Creation and Readability to Manager

--- src/Database/Managers/UserManager.php
+++ src/Database/Managers/UserManager.php
@@ -4,10 +4,39 @@ namespace App\Database\Managers;
 
 use App\Core\Manager;
 use App\Database\Models\User;
+use PDOException;
 
 class UserManager extends Manager
 {
     public function __construct() {
+        $this->setTable('user');
+        $this->getConnection();
+    }
 
+    public function find(string $id): User | null {
+        $match = $this->getOne($id);
+
+        if ($match) {
+            return new User($match['id'], $match['username'], $match['password']);
+        }
+
+        return null;
+    }
+
+    public function create(string $id, string $username, string $password): bool | PDOException {
+        $query = $this->_connexion->prepare('INSERT INTO :table (id, username, password) VALUES (:id, :username, :password)');
+
+        try {
+            $query->execute([
+                'table' => $this->getTable(),
+                'id' => $id,
+                'username' => $username,
+                'password' => $password,
+            ]);
+
+            return true;
+        } catch(PDOException $e) {
+            return false;
+        }
     }
 }
