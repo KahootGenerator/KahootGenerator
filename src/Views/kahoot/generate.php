5ca4dcf42f39bceea29ecce22ad0ececc8df521a
IMTR0J4N <jjorgebeau@edenschool.fr>
2024-06-12 14:15:38 +0200

    add waiting animation html

--- src/Views/kahoot/generate.php
+++ src/Views/kahoot/generate.php
@@ -1,9 +1,10 @@
 <?php
+
 use App\Helper;
 
 ?>
 
-<form action="/kahoot/generate/attempt" method="POST" class="form">
+<form action="/kahoot/generate/attempt" method="POST" class="form" id="formGen">
     <h1><?= $data["title"] ?></h1>
 
     <div class="form-field--full">
@@ -30,16 +31,13 @@ use App\Helper;
                 <summary>
                     <input type="radio" name="lang" title="Choisir une langue" checked>
                     <?php foreach ($languages as $language) { ?>
-                        <input type="radio" name="lang" id="lang_<?= Helper::escape($language->getId()) ?>"
-
-                            title="<?= Helper::escape($language->getLibelle()) ?>" value="<?= $language->getId(); ?>">
+                        <input type="radio" name="lang" id="lang_<?= Helper::escape($language->getId()) ?>" title="<?= Helper::escape($language->getLibelle()) ?>" value="<?= $language->getId(); ?>">
                     <?php } ?>
                 </summary>
                 <ul>
                     <?php foreach ($languages as $language) { ?>
                         <li>
-                            <label
-                                for="lang_<?= Helper::escape($language->getId()) ?>"><?= Helper::escape($language->getLibelle()) ?></label>
+                            <label for="lang_<?= Helper::escape($language->getId()) ?>"><?= Helper::escape($language->getLibelle()) ?></label>
                         </li>
                     <?php } ?>
                 </ul>
@@ -59,15 +57,13 @@ use App\Helper;
                     <input type="radio" name="diff" title="Choisir une difficulté" checked>
 
                     <?php foreach ($difficulties as $difficulty) { ?>
-                        <input type="radio" name="diff" id="diff_<?= Helper::escape($difficulty->getId()) ?>"
-                            title="<?= Helper::escape($difficulty->getLibelle()) ?>" value="<?= $difficulty->getId(); ?>">
+                        <input type="radio" name="diff" id="diff_<?= Helper::escape($difficulty->getId()) ?>" title="<?= Helper::escape($difficulty->getLibelle()) ?>" value="<?= $difficulty->getId(); ?>">
                     <?php } ?>
                 </summary>
                 <ul>
                     <?php foreach ($difficulties as $difficulty) { ?>
                         <li>
-                            <label
-                                for="diff_<?= Helper::escape($difficulty->getId()) ?>"><?= Helper::escape($difficulty->getLibelle()) ?></label>
+                            <label for="diff_<?= Helper::escape($difficulty->getId()) ?>"><?= Helper::escape($difficulty->getLibelle()) ?></label>
                         </li>
                     <?php } ?>
                 </ul>
@@ -98,4 +94,51 @@ use App\Helper;
     </div>
 
 </form>
+<div class="main-container" id="waitingContainer">
+    <h1>Génération du Kahoot</h1>
+    <div class="spinner-container">
+        <!-- <svg class="ip" viewBox="0 0 256 128" width="256px" height="128px" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <linearGradient id="grad1" x1="0" y1="0" x2="1" y2="0">
+                    <stop offset="0%" stop-color="#5ebd3e" />
+                    <stop offset="33%" stop-color="#ffb900" />
+                    <stop offset="67%" stop-color="#f78200" />
+                    <stop offset="100%" stop-color="#e23838" />
+                </linearGradient>
+                <linearGradient id="grad2" x1="1" y1="0" x2="0" y2="0">
+                    <stop offset="0%" stop-color="#e23838" />
+                    <stop offset="33%" stop-color="#973999" />
+                    <stop offset="67%" stop-color="#009cdf" />
+                    <stop offset="100%" stop-color="#5ebd3e" />
+                </linearGradient>
+            </defs>
+            <g fill="none" stroke-linecap="round" stroke-width="16">
+                <g class="ip__track" stroke="#ddd">
+                    <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
+                    <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
+                </g>
+                <g stroke-dasharray="180 656">
+                    <path class="ip__worm1" stroke="url(#grad1)" stroke-dashoffset="0" d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
+                    <path class="ip__worm2" stroke="url(#grad2)" stroke-dashoffset="358" d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
+                </g>
+            </g>
+        </svg>
+        <div class="pl1">
+            <div class="pl1__a"></div>
+            <div class="pl1__b"></div>
+            <div class="pl1__c"></div>
+        </div> -->
+        <div class="pencil">
+            <div class="pencil__ball-point"></div>
+            <div class="pencil__cap"></div>
+            <div class="pencil__cap-base"></div>
+            <div class="pencil__middle"></div>
+            <div class="pencil__eraser"></div>
+        </div>
+        <div class="line"></div>
+    </div>
+    <span id="generationText">Votre kahoot en en cours de génération. Merci de patienter</span>
+</div>
+</div>
 <script type="module" src="/js/components/select.js"></script>
+<script type="module" src="/js/apiWaiting.js"></script>
