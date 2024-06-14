f03bd704b15c3aec62b911c6dad5174308ec59ea
corbiobio <edarnand@edenschool.fr>
2024-06-14 11:15:29 +0200

    update show html

--- src/Views/kahoot/show.php
+++ src/Views/kahoot/show.php
@@ -54,7 +54,8 @@ if (empty($kahoot)) {
                         </div>
                     </article>
                     <div class="question">
-                        <p class="question-title" id="title-<?= $question->getId(); ?>" contenteditable="plaintext-only"><?= Helper::escape($question->getQuestion()); ?></p>
+                        <p class="question-title" id="title-<?= $question->getId(); ?>" contenteditable="plaintext-only">
+                            <?= Helper::escape($question->getQuestion()); ?></p>
                         <div class="responses-wrapper">
                             <?php foreach ($question->getAnswers() as $answer) { ?>
                                 <div class="response">
@@ -96,9 +97,9 @@ if (empty($kahoot)) {
                 </div>
             <?php } ?>
         </div>
-        <div class="buttons">
-            <button class="button-purple button-xl" id="createNewQuestion">Ajouter une nouvelle question</button>
-            <a class="button-orange button-download" href="/kahoot/<?= $kahoot->getId() ?>/download">Télécharger votre
+        <button class="button-lightblue button-xl" id="createNewQuestion">Créer une nouvelle question</button>
+        <div class="download-block">
+            <a class="button-orange" href="/kahoot/<?= $kahoot->getId() ?>/download">Telecharger votre
                 Kahoot !</a>
             <button class="button-purple" id="save">Sauvegarder vos modifications !</button>
         </div>
